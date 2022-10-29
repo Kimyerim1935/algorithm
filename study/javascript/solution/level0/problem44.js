c8a69ae010ec19e6801d921ac8c6869f5f753f22
Kimyerim1935 <kimyerim1935@naver.com>
2022-10-29 23:29:56 +0900

    add: 팩토리얼

---
+++ study/javascript/solution/level0/problem44.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0,
+        count = 1,
+        i = 1;
+    while (count <= n) {
+        i++, answer++;
+        count = count * i;
+    }
+
+    return answer;
+}
