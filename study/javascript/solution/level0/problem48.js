19eb845b96a6744a871b2252d6ab376815a346eb
Kimyerim1935 <kimyerim1935@naver.com>
2022-10-30 14:07:21 +0900

    add: 소인수분해

---
+++ study/javascript/solution/level0/problem48.js
@@ -0,0 +1,14 @@
+function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function solution(n) {
+    const answer = [];
+    for (let i = 2; i <= n; i++) {
+        if (n % i === 0 && isPrime(i)) answer.push(i);
+    }
+    return answer;
+}
