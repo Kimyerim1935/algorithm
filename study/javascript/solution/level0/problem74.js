0413f8aa7bfbe0ff7a42e335eb61ef0f43046528
Kimyerim1935 <kimyerim1935@naver.com>
2022-11-07 23:57:30 +0900

    add: 잘라서 배열로 저장하기

---
+++ study/javascript/solution/level0/problem74.js
@@ -0,0 +1,7 @@
+function solution(my_str, n) {
+    let result = [];
+    for (let i = 0; i < my_str.length / n; i++) {
+        result = [...result, my_str.slice(i * n, i * n + n)];
+    }
+    return result;
+}
