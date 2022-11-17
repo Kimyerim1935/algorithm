b99a1e96e93fc4fdc47c086dadb3ff364ac98a50
Kimyerim1935 <kimyerim1935@naver.com>
2022-11-17 18:07:11 +0900

    add: 옹알이(1)

---
+++ study/javascript/solution/level0/problem91.js
@@ -0,0 +1,19 @@
+function solution(babbling) {
+    const wordSet = new Set(["aya", "ye", "woo", "ma"]);
+    let result = 0;
+
+    for (const word of babbling) {
+        let remainWord = "";
+        let prevWord = "";
+        for (const char of word) {
+            remainWord += char;
+            if (wordSet.has(remainWord)) {
+                if (remainWord === prevWord) break;
+                prevWord = remainWord;
+                remainWord = "";
+            }
+        }
+        if (remainWord === "") result++;
+    }
+    return result;
+}
