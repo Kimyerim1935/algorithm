77e6310f92d727c6460a10f010ca2ec2365b55bb
Kimyerim1935 <kimyerim1935@naver.com>
2023-01-19 21:23:06 +0900

    add: level1 이상한 문자 만들기

---
+++ study/javascript/solution/level1/problem19.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    let x = s.split(" ");
+    let answer = [];
+    for (let i = 0; i < x.length; i++) {
+        answer.push(
+            x[i]
+                .split("")
+                .map((cur, j) => (j % 2 ? cur.toLowerCase() : cur.toUpperCase()))
+                .join("")
+        );
+    }
+    return answer.join(" ");
+}
