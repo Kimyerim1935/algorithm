405d9c5a226af2f053f51ce0233b6dcbfc04899b
Rim <61038091+Kimyerim1935@users.noreply.github.com>
2024-01-30 12:45:32 +0900

    [Bronze I] Title: 단어 공부, Time: 284 ms, Memory: 21020 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toUpperCase();
+        int maxValue = 0;
+        char maxChar = 0;
+        
+        int[] alphabetArr = new int[26];
+        
+        for(int i=0; i<str.length(); i++){
+            alphabetArr[str.charAt(i)-'A']++;
+        }        
+        
+        for(int i=0; i<26; i++){
+            int compare = alphabetArr[i]- maxValue;
+            if(compare > 0){
+                 maxValue = alphabetArr[i]; 
+                 maxChar = (char)('A'+i);
+            } else if(compare == 0 ) maxChar ='?';            
+        }        
+        System.out.println(maxChar);        
+    }    
+}
