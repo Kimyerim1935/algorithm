8752b74103a07f54d88a30bae13e7cacd143141f
Rim <61038091+Kimyerim1935@users.noreply.github.com>
2024-08-13 22:00:40 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long a = in.nextInt();
+        System.out.println((a*(a-1)*(a-2))/6);
+        System.out.println(3);
+    }
+}
