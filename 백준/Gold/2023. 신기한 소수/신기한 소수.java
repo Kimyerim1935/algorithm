d1a184ba8582333a87f5323144bb02af3264faff
Rim <61038091+Kimyerim1935@users.noreply.github.com>
2024-08-15 23:20:25 +0900

    [Gold V] Title: 신기한 소수, Time: 188 ms, Memory: 18636 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] firstPrimeArr = {2, 3, 5, 7};
+    static int[] notFirstPrimeArr = {1, 3, 7, 9};
+
+    public static void main(String[] args) {
+        int number = Integer.parseInt(new Scanner(System.in).nextLine());
+        for (int i = 0; i < 4; i++) {
+            per(number, new StringBuilder().append(firstPrimeArr[i]));
+        }
+    }
+
+    static void per(int maxCount, StringBuilder currentStr) {
+        if (maxCount == currentStr.toString().length()) {
+            System.out.println(currentStr);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            int currentNumber = Integer.parseInt(currentStr.toString() + notFirstPrimeArr[i]);
+            if (isPrime(currentNumber)) {
+                per(maxCount, currentStr.append(notFirstPrimeArr[i]));
+                currentStr.setLength(currentStr.length() - 1);
+            }
+        }
+    }
+
+    static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
